6cb42f88f169561db1cf219ff29dcf94d6b9061a
Ben <127059162+bencsci@users.noreply.github.com>
2025-05-20 20:20:41 -0300

    Update tribonacci.py

--- Dynamic-Programming/097-tribonacci/tribonacci.py
+++ Dynamic-Programming/097-tribonacci/tribonacci.py
@@ -3,4 +3,15 @@ def tribonacci(n):
 
   def dfs(n):
 
-    
+    if n in memo:
+      return memo[n]
+
+    if n == 0 or n == 1:
+      return 0
+    elif n == 2:
+      return 1
+
+    memo[n] = dfs(n-1) + dfs(n-2) + dfs(n-3)
+    return memo[n]
+
+  return dfs(n)
