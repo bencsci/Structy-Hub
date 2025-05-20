f3e730273849ce4d2f1591cfacf73d276304b404
Ben <127059162+bencsci@users.noreply.github.com>
2025-05-20 19:21:37 -0300

    Update fib.py

--- Dynamic-Programming/096-fib/fib.py
+++ Dynamic-Programming/096-fib/fib.py
@@ -1,2 +1,17 @@
 def fib(n):
-  pass # todo
+  memo = {}
+
+  def dfs(n):
+    if n in memo:
+      return memo[n]
+
+    if n == 0:
+      return 0
+    elif n == 1:
+      return 1
+
+    memo[n] = dfs(n-1) + dfs(n-2)
+    return memo[n]
+
+  return dfs(n)
+    
