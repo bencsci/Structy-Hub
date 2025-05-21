6c3784e71d7bd7b9977db63c37c89bc8dcfdcbc5
Ben <127059162+bencsci@users.noreply.github.com>
2025-05-21 16:21:59 -0300

    Solved 'tribonacci' (python3)

--- Dynamic-Programming/097-tribonacci/tribonacci.py
+++ Dynamic-Programming/097-tribonacci/tribonacci.py
@@ -1,17 +1,2 @@
 def tribonacci(n):
-  memo = {}
-
-  def dfs(n):
-
-    if n in memo:
-      return memo[n]
-
-    if n == 0 or n == 1:
-      return 0
-    elif n == 2:
-      return 1
-
-    memo[n] = dfs(n-1) + dfs(n-2) + dfs(n-3)
-    return memo[n]
-
-  return dfs(n)
+  pass # todo
