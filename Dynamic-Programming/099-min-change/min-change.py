756dc08c765f8318e9f312289da2dba37fe9cff8
Ben <127059162+bencsci@users.noreply.github.com>
2025-05-21 17:13:36 -0300

    Solved 'min change' (python3)

--- Dynamic-Programming/099-min-change/min-change.py
+++ Dynamic-Programming/099-min-change/min-change.py
@@ -1,28 +1,19 @@
-# subtract each num in coins if a - c >= 0
-# keep tract of the subtraction list per branch
-# return the min numbers of num where the ans returns True
-
 def min_change(amount, coins):
   memo = {}
-  
+
   def dfs(amount):
     if amount < 0:
       return float('inf')
-    
-    if amount in memo:
-      return memo[amount]
 
     if amount == 0:
       return 0
-      
+
     minC = float('inf')
-    for num in coins:
-      if amount - num >= 0:
-        minC = min(minC, dfs(amount - num) + 1)
-        memo[amount] = minC
+    for coin in coins:
+      minC = min(minC, dfs(amount - coin) + 1)
 
     memo[amount] = minC
     return minC
 
   ans = dfs(amount)
-  return ans if ans != float('inf') else -1
+  return ans if ans != float('inf') else - 1
