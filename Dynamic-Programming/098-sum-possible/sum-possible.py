94f1053c8278e2d05f1cf7e8a640d0a2c24fd43d
Ben <127059162+bencsci@users.noreply.github.com>
2025-05-23 16:04:11 -0300

    Solved 'sum possible' (python3)

--- Dynamic-Programming/098-sum-possible/sum-possible.py
+++ Dynamic-Programming/098-sum-possible/sum-possible.py
@@ -2,22 +2,21 @@ def sum_possible(amount, numbers):
   memo = {}
 
   def dfs(amount):
-
     if amount in memo:
       return memo[amount]
+    
+    if amount < 0:
+      return False
     
     if amount == 0:
       return True
 
     for num in numbers:
-      if amount - num >= 0:
-        if dfs(amount - num):
-          memo[amount] = True
-          return True
+      if dfs(amount - num):
+        memo[amount] = True
+        return True
 
     memo[amount] = False
-    return False
+    return memo[amount]
 
   return dfs(amount)
-
-  
