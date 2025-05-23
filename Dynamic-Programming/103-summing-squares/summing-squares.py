c99b2cb4a86dd15c0967a19bddaf9f88aa07ba2c
Ben <127059162+bencsci@users.noreply.github.com>
2025-05-23 20:44:01 -0300

    Solved 'summing squares' (python3)

---
+++ Dynamic-Programming/103-summing-squares/summing-squares.py
@@ -0,0 +1,48 @@
+# get all perfect squares up to that number
+# subtract each perfect square from that number 
+# keep track of the count of recursive calls
+# return them in of thoses calls where the base case is 0
+
+def summing_squares(n):
+  memo  = {}
+  
+  def dfs(n):
+    squares = perfect_squares(n)
+
+    if n in memo:
+      return memo[n]
+    
+    if n < 0:
+      return float('inf')
+
+    if n == 0:
+      return 0
+
+    minSq = float('inf')
+    for square in squares:
+      minSq = min(minSq, dfs(n-square)+1)
+      memo[n] = minSq
+
+    memo[n] = minSq
+    return  memo[n]
+
+  return dfs(n)
+
+def perfect_squares(n):
+  squares = []
+
+  if n == 1:
+    return [1]
+  
+  for i in range(1, n):
+    square = i * i
+    if square > n:
+      return squares
+    squares.append(square)
+
+  return squares
+
+# test = summing_squares(12)
+1
+# print(perfect_squares(1))
+# print(test)
