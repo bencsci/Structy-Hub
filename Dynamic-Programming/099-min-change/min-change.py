e2a21a3c0094b300c72e7ad90d5642a9c0cea866
Ben <127059162+bencsci@users.noreply.github.com>
2025-05-24 21:19:57 -0300

    Solved 'min change' (python3)

--- Dynamic-Programming/099-min-change/min-change.py
+++ Dynamic-Programming/099-min-change/min-change.py
@@ -1,22 +1,37 @@
+# subtract each coin in coins from the amount
+# keep count of each recursive call for each branch
+# if amount == 0 return the total count for that branch
+# compare different successful min values for each branch
+# return the min value
+
+# Optimization: Use Memo to store seen values
+# reuse those values if a calculation reappears
+
+# time o(a*c) -> space o(n)
+
 def min_change(amount, coins):
+
   memo = {}
-  
+
   def dfs(amount):
+
     if amount in memo:
       return memo[amount]
     
     if amount < 0:
       return float('inf')
-
+    
     if amount == 0:
       return 0
 
     minC = float('inf')
     for coin in coins:
-      minC = min(minC, dfs(amount - coin) + 1)
-      memo[amount] = minC
-      
+      minC = min(minC, dfs(amount-coin) + 1)
+
     memo[amount] = minC
     return memo[amount]
 
   return dfs(amount)
+      
+
+    
