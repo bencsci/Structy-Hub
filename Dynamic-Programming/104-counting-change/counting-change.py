83ee6400160d0ed5508a4387f184f37810d2fc4b
Ben <127059162+bencsci@users.noreply.github.com>
2025-05-26 17:09:17 -0300

    Solved 'counting change' (python3)

--- Dynamic-Programming/104-counting-change/counting-change.py
+++ Dynamic-Programming/104-counting-change/counting-change.py
@@ -1,27 +1,21 @@
-# create a tree where each level represents the value of the coin from 1 ... coin
-# on each level we can choose a quantity for using that coin from 0 ... coin
-# if we reach zero then we can count it as a successfull branch
-
-#                     4
-#    x0/    x1/     x2|    x3\     x4\ number of times we used 1 cent coin
-#   4       3        2      1       0 
-# etc.
+# Create recursive tree with each level representing the value of the coins
+# and take 0 ... n amount of that coin while subtracting qty x coin for each node
+# 
 
 def counting_change(amount, coins):
   memo = {}
 
-  
   def dfs(amount, i):
     key = (amount, i)
 
     if key in memo:
       return memo[key]
- 
-    if amount == 0:
-      return 1
-
+    
     if i == len(coins):
       return 0
+    
+    if amount == 0:
+      return 1
 
     coin = coins[i]
 
@@ -34,4 +28,4 @@ def counting_change(amount, coins):
     return memo[key]
 
   return dfs(amount, 0)
-  
+      
