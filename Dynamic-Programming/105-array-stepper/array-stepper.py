2eb6769824ecd9941d6728bfc631605310598e1b
Ben <127059162+bencsci@users.noreply.github.com>
2025-05-27 18:15:18 -0300

    Solved 'array stepper' (python3)

--- Dynamic-Programming/105-array-stepper/array-stepper.py
+++ Dynamic-Programming/105-array-stepper/array-stepper.py
@@ -1,43 +1,29 @@
-# Create a recursive tree with each call being a decision from 1 .... step
-# if our step is 0: return False
-# if we move out of bounds return: False
-# if we reach the end of the array: return True
-# Otherwise return False
-
-#                       2
-#               1/             2\
-#               4               2    
-#         1/ 2/ 3\ 4\        1/  2\
-#  etc.
-
-# Optimization: use memoization to store previously calculated values
-
+# Create a recursive Tree, where each node represents an the current index
+# Each branch is the step at the index
+# If our current step is 0: return False
+# If we are >= len(numbers) - 1: return True -> this means we reached the end of the array
 
 def array_stepper(numbers):
-
   memo = {}
   
-  def dfs(i):
-
-    if i >= len(numbers):
-      return False
-
-    if i == len(numbers) - 1:
-      return True
+  def dfs(idx):
+    if idx in memo:
+      return memo[idx]
     
-    step = numbers[i]
+    if idx >= len(numbers) - 1:
+      return True
 
-    if step == 0:
+    curr_step = numbers[idx]
+
+    if curr_step == 0:
       return False
 
-    for choice in range(1, step+1):
-      if dfs(i + choice):
+    for step in range(1, curr_step + 1):
+      if dfs(idx + step):
+        memo[idx] = True
         return True
 
-    return False
+    memo[idx] = False
+    return memo[idx]
 
   return dfs(0)
-
-    
-
-  
