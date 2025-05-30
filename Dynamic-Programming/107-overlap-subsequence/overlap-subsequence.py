5aebab4d0cc0a48f972cf396e9089972e9736193
Ben <127059162+bencsci@users.noreply.github.com>
2025-05-30 15:23:21 -0300

    Solved 'overlap subsequence' (python3)

--- Dynamic-Programming/107-overlap-subsequence/overlap-subsequence.py
+++ Dynamic-Programming/107-overlap-subsequence/overlap-subsequence.py
@@ -1,4 +1,16 @@
+# have two pointers starting at each string
+# if the string equal then increase both pointers, increase count
+# else increase on or the other pointer
+
+# 
+
+#  1
+# dogs
+#     2 
+# daogt
+
 def overlap_subsequence(string_1, string_2):
+
   memo = {}
   
   def dfs(s1, s2):
@@ -7,14 +19,19 @@ def overlap_subsequence(string_1, string_2):
 
     if key in memo:
       return memo[key]
-    
-    if s1 > len(string_1) - 1 or s2 > len(string_2) - 1:
+
+    if s1 >= len(string_1)  or s2 >= len(string_2):
       return 0
     
     if string_1[s1] == string_2[s2]:
-      return 1 + dfs(s1+1, s2+1)
+      return 1 + dfs(s1 + 1, s2 + 1)
 
-    memo[key] = max(dfs(s1+1, s2), dfs(s1, s2+1))
-    return memo[key]
+    count = 0
+    count += max(dfs(s1 + 1, s2), dfs(s1, s2 + 1))
+
+    return count
 
   return dfs(0, 0)
+
+test = overlap_subsequence("xcyats", "criaotsi") # -> 4
+print(test)
