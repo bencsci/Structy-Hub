a59f51313cf4d094b3c0937e44b3b84590f4c746
Ben Le <127059162+bencsci@users.noreply.github.com>
2025-06-15 02:02:30 -0300

    Solved 'sum possible' (python3)

--- Dynamic-Programming/098-sum-possible/sum-possible.py
+++ Dynamic-Programming/098-sum-possible/sum-possible.py
@@ -1,27 +1,24 @@
-# Create a recursive decision tree where each node is the amount - decision
-# Go through each num in numbers in subtract from the amount
-# if amount is negative reutrn False
-# amount == zero return True
-
 def sum_possible(amount, numbers):
+
   memo = {}
   
   def dfs(amount):
-    if amount in memo:
-      return memo[amount]
-    
-    if amount < 0:
-      return False
 
+    if amount in memo:
+      return amount[memo]
+    
     if amount == 0:
       return True
 
+    if amount < 0:
+      return False
+
     for num in numbers:
       if dfs(amount - num):
-        memo[amount] = True
+        amount[memo] = True
         return True
-
-    memo[amount] = False
-    return memo[amount]
+        
+    amount[memo] = False
+    return amount[memo]
 
   return dfs(amount)
