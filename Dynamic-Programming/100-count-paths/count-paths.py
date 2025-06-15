d02253e7d04f514c3fc9c7a4d033e6ea5b913e58
Ben Le <127059162+bencsci@users.noreply.github.com>
2025-06-15 20:59:21 -0300

    Solved 'count paths' (python3)

--- Dynamic-Programming/100-count-paths/count-paths.py
+++ Dynamic-Programming/100-count-paths/count-paths.py
@@ -1,31 +1,40 @@
-# Go through each decision to move own or right
-# once our grid position == the bottom right corner increase our count
+# Traverse the grid moving down or to the right, if we reach the bottom right corner increase the # count by 1
+# If we are out of bounds or hit an x return 0
 
 def count_paths(grid):
-  rows = len(grid)
-  cols = len(grid[1])
   memo = {}
+  rows = len(grid)
+  cols = len(grid[0])
   
   def dfs(row, col):
     pos = (row, col)
 
     if pos in memo:
       return memo[pos]
-    
-    if not (0 <= row < rows) or not (0 <= col < cols):
+
+    if not (0 <= row < rows and 0 <= col < cols):
       return 0
 
-    if grid[row][col] != 'O':
+    if grid[row][col] == 'X':
       return 0
 
     if row == rows - 1 and col == cols - 1:
       return 1
 
     count = 0
-    count += dfs(row + 1, col)
-    count += dfs(row, col + 1)
+    count += dfs(row+1, col)
+    count += dfs(row, col+1)
 
     memo[pos] = count
     return memo[pos]
 
-  return dfs(0, 0)
+  return dfs(0,0)
+
+grid = [
+  ["O", "O", "X"],
+  ["O", "O", "O"],
+  ["O", "O", "O"],
+]
+count_paths(grid) # -> 5
+test = count_paths(grid) # -> 2
+print(test)
