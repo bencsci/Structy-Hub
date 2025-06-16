b30308580fdcae42c9688e0d2494c916c82a6f5c
Ben Le <127059162+bencsci@users.noreply.github.com>
2025-06-16 18:57:03 -0300

    Solved 'counting change' (python3)

--- Dynamic-Programming/104-counting-change/counting-change.py
+++ Dynamic-Programming/104-counting-change/counting-change.py
@@ -1,16 +1,18 @@
-# Create a recursive tree where each level represents the value of the coins 
-# starting from 1 ... amount
-# each branch of the tree represents the quantity and will pass in the new amount
-# amount - (quantity * coin value)
-# if amount is 0: return 1 
-# if amount < 0: return 0
-
-# Optimization: use memoization to store recomputed values
-
+# Create a decision tree where each level represents the value of the coin
+# 1 .. n and each branch is choosing 0 .. n coins
+# each node value we subtract the qty * coin from our amount until we reach 0
+# count the number of times we reach 0
+# have an index so we can traverse through the coins for each call
+# [1, 2, 3]
+#                    4
+#       0x/   1x/  2x|  3x\  4x\    1 cent coins
+#       4     3    2     1    0
 def counting_change(amount, coins):
+
   memo = {}
   
   def dfs(amount, i):
+
     key = (amount, i)
 
     if key in memo:
@@ -19,17 +21,22 @@ def counting_change(amount, coins):
     if amount == 0:
       return 1
 
-    if i == len(coins):
+    if i >= len(coins):
       return 0
 
     coin = coins[i]
 
     count = 0
     for qty in range((amount // coin) + 1):
-      new = amount - (qty * coin)
-      count += dfs(new, i + 1)
+      next = amount - (qty * coin)
+      count += dfs(next, i+1)
 
     memo[key] = count
     return memo[key]
 
   return dfs(amount, 0)
+      
+
+    
+
+    
