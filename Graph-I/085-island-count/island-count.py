482d2994c53a48d9b5edd80262243632b9c57ec5
Ben Le <127059162+bencsci@users.noreply.github.com>
2025-06-23 16:55:50 -0300

    Solved 'island count' (python3)

--- Graph-I/085-island-count/island-count.py
+++ Graph-I/085-island-count/island-count.py
@@ -1,34 +1,27 @@
-# traverse the grid, if we find land
-# dfs search until we explored all the land, mark the visited land
-# return True to count the island
 def island_count(grid):
   rows = len(grid)
   cols = len(grid[0])
 
   def dfs(row, col):
-    if not (0 <= row < rows) or not (0 <= col < cols):
+    if not (0 <= row < rows and 0 <= col < cols):
       return False
 
-    if grid[row][col] == 'W':
-      return False
-
-    if grid[row][col] == 'V':
+    if grid[row][col] != 'L':
       return False
 
     grid[row][col] = 'V'
 
-    dfs(row+1, col)
-    dfs(row-1, col)
-    dfs(row, col+1)
-    dfs(row, col-1)
+    dfs(row + 1, col)
+    dfs(row, col + 1)
+    dfs(row - 1, col)
+    dfs(row, col - 1)
 
     return True
 
   count = 0
-  for row in range(rows):
-    for col in range(cols):
-      if dfs(row, col):
+  for row in rows:
+    for col in cols:
+      if df(row, col):
         count += 1
 
   return count
-        
