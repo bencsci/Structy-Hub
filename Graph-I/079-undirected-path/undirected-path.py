d6028d0c203d6d0f322eb838347d11726cde9c61
Ben Le <127059162+bencsci@users.noreply.github.com>
2025-06-23 15:55:24 -0300

    Solved 'undirected path' (python3)

--- Graph-I/079-undirected-path/undirected-path.py
+++ Graph-I/079-undirected-path/undirected-path.py
@@ -2,28 +2,29 @@ from collections import defaultdict
 
 def undirected_path(edges, node_A, node_B):
   graph = buildGraph(edges)
-  v = set()
+  visited = set()
   
   def dfs(src):
     if src == node_B:
       return True
-
-    for n in graph[src]:
-      if n not in v and dfs(n):
-        v.add(n)
-        return True
-
-    return False
     
+    for neighbour in graph[src]:
+      if neighbour not in visited:
+        visited.add(neighbour)
+        if dfs(neighbour):
+          return True
+      else:
+        continue
+              
+    return False
   return dfs(node_A)
+    
 
 def buildGraph(edges):
-  graph = defaultdict(list) 
-  
+  graph = defaultdict(list)
+
   for edge in edges:
-    graph[edge[0]].append(edge[1])
-    graph[edge[1]].append(edge[0])
+    graph[edge[0]] = edge[1]
+    graph[edge[1]] = edge[0]
 
   return graph
-
-  
