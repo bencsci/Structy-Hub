60dc6dc0c32e5e3feae2cd05229b67929c987a25
Ben Le <127059162+bencsci@users.noreply.github.com>
2025-06-23 16:28:37 -0300

    Solved 'largest component' (python3)

--- Graph-I/082-largest-component/largest-component.py
+++ Graph-I/082-largest-component/largest-component.py
@@ -1,26 +1,19 @@
-# dfs traverse through each neighbour, mark them as visited
-# count each neighbour until dfs ends. return the size of the component and 
-# compare with max from previous max. Return max
-
 def largest_component(graph):
-  largest = 0
   v = set()
-  
-  def dfs(src):
-    if src is None:
-      return 0
 
+  def dfs(src):
     if src in v:
       return 0
 
-    v.add(src)
     count = 1
-    for n in graph[src]:
-      count += dfs(src)
-    
+    v.add(src)
+    for node in graph[src]:
+      count += dfs(node)
+
     return count
 
-  for n in graph:
-      largest = max(largest, dfs(n))
+  res = float('-inf')
+  for node in graph:
+    res = max(res, dfs(node))
 
-  return largest
+  return res
