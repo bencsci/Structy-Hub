b0c6a97b5895a6c52fd26e2155717434c561162f
Ben Le <127059162+bencsci@users.noreply.github.com>
2025-07-02 15:52:52 -0300

    Solved 'weighted graph min path' (python3)

---
+++ Graph-II/177-weighted-graph-min-path/weighted-graph-min-path.py
@@ -0,0 +1,24 @@
+# Traverse through the graph using dfs and mark visited nodes
+# If we reach the target node return 0
+# add the costs of each node each time we travel to a new node
+# compared the cost and reuturn min
+
+def weighted_graph_min_path(graph, src, dst):
+  v = set()
+
+  def dfs(src):
+    if src == dst:
+      return 0
+
+    if src in v:
+      return float('inf')
+
+    v.add(src)
+    weight = float('inf')
+    for node, w in graph[src].items():
+      weight = min(weight, dfs(node)+w)
+
+    v.remove(src)
+    return weight
+
+  return dfs(src)
