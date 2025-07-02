2fcbbccf4efe504e9233b856080994800d46b098
Ben Le <127059162+bencsci@users.noreply.github.com>
2025-07-02 17:43:48 -0300

    Solved 'tree levels' (python3)

---
+++ Binary-Tree-I/065-tree-levels/tree-levels.py
@@ -0,0 +1,31 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+from collections import deque
+
+def tree_levels(root):
+  if root is None:
+    return []
+
+  levels = []
+  q = deque([root])
+
+  while q:
+    length = len(q)
+    level = []
+    
+    for _ in range(length):
+      curr = q.popleft()
+      level.append(curr.val)
+      
+      if curr.left:
+        q.append(curr.left)
+      if curr.right:
+        q.append(curr.right)
+        
+    levels.append(level)
+
+  return levels
