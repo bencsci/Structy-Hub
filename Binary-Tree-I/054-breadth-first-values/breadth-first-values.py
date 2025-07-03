f94a7cc9b9556ad60b4f5ca0e8d698927e1b0a67
Ben Le <127059162+bencsci@users.noreply.github.com>
2025-07-03 14:36:33 -0300

    Solved 'breadth first values' (python3)

--- Binary-Tree-I/054-breadth-first-values/breadth-first-values.py
+++ Binary-Tree-I/054-breadth-first-values/breadth-first-values.py
@@ -3,6 +3,7 @@
 #     self.val = val
 #     self.left = None
 #     self.right = None
+
 from collections import deque
 
 def breadth_first_values(root):
@@ -10,7 +11,14 @@ def breadth_first_values(root):
     return []
 
   q = deque([root])
-
+  res = []
+  
   while q:
     curr = q.popleft()
-    
+    res.append(curr.val)
+    if curr.left:
+      q.append(curr.left)
+    if curr.right:
+      q.append(curr.right)
+
+  return res
